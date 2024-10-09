08f935902322f6b01d4c17b0e9c72f88ee098a0a
mohammadhusainmasi <mohammadhusain0538@gmail.com>
2024-10-09 09:58:37 +0530

    new 08/10

--- first.js
+++ first.js
@@ -1,13 +1,34 @@
-function myfunction() {
-    console.log("welcome to mahertour!");
-    console.log("we are learning js!");
-    console.log("welcome to mahertour!");
-    console.log("we are learning js!");
+// function myfunction(msg, n) {
+    //parameter -> input
+    // console.log(msg * n);
+// }
+
+// myfunction("i love JS", 100); //argument
+
+//function -> 2 numbers, sum
+
+// function sum(x, y) {
+//    //local variables -> 
+//    s = x + y;
+//    console.log("before return");
+//    return s;
+//    console.log("after return");
+// }
+// let val = sum(3, 4);
+// console.log(val);
+
+
+// sum function
+function sum(a, b) {
+    return a + b;
 }
 
-myfunction();
-myfunction();
-myfunction();
+const arrowSum =(a, b) => {
+    console.log(a + b);
+};
 
-
-asdf
+//multiplication function
+function mul(a,b) {
+    return a * b; 
+    return a * b;
+}
