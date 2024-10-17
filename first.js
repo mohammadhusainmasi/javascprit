ef39b633ebe7d7e5651862961d71617b04dbd6ac
mohammadhusainmasi <mohammadhusain0538@gmail.com>
2024-10-17 13:27:18 +0530

    dom 16,17/10

--- first.js
+++ first.js
@@ -1,30 +1,7 @@
-// let marks = [97, 77, 40, 60, 43, 67];
+// let heading = document.getElementById("heading"); //h1
+// console.log(heading);
 
-// let toppers = marks.filter((val) =>{
-//    return val > 50;
-// });
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
 
-// console.log(toppers);
-
-
-let n = prompt("enter a number : ");
-
-let arr =[];
-
-for (let i =1; i<=n; i++) {
-    arr[i-1] = i; 
-}
-
-console.log(arr);
-
-let sum = arr.reduce((res,curr) => {
-    return res + curr;
-})
-
-console.log("sum = ",sum);
-
-let factorial = arr.reduce((res, curr) => {
-    return res * curr;
-});
-
-console.log("factorial = ", factorial);
--- new js/first.js
+++
@@ -1,8 +0,0 @@
-fullname = "mohammadhusain";
-age = 19
-price = 403.50
-radius = 40
-console.log(radius);  
-console.log(radius);
-console.log(radius);
-console.log(radius);
