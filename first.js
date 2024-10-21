95d69aee7101b912f0e278792dce03c2fb18ee0b
mohammadhusainmasi <mohammadhusain0538@gmail.com>
2024-10-21 10:41:29 +0530

    dom manipulation 21/10

--- first.js
+++ first.js
@@ -1,7 +1,2 @@
-// let heading = document.getElementById("heading"); //h1
-// console.log(heading);
-
-let headings = document.getElementsByClassName("heading-class");
-console.dir(headings);
-console.log(headings);
-
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
