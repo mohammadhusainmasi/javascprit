5f89cbda326eee84b478d01e5e07d631afc805e1
mohammadhusainmasi <mohammadhusain0538@gmail.com>
2024-11-25 09:46:10 +0530

    classes & objects 25/11

--- classes & object/app.js
+++ classes & object/app.js
@@ -1,117 +1,123 @@
-// // // // // const student = {
-// // // // //     fullName : "Mohammadhusain Masi",
-// // // // //     marks : 99.99,
-// // // // //     printMarks: function () {
-// // // // //         console.log("marks = ", this.marks); //student.marks
-// // // // //     }, 
+// // // // // // const student = {
+// // // // // //     fullName : "Mohammadhusain Masi",
+// // // // // //     marks : 99.99,
+// // // // // //     printMarks: function () {
+// // // // // //         console.log("marks = ", this.marks); //student.marks
+// // // // // //     }, 
+// // // // // // };
+
+// // // // // const employee = {
+// // // // //     calcTax () {
+// // // // //         console.log("tax rate is 10%");
+// // // // //     },
 // // // // // };
 
-// // // // const employee = {
-// // // //     calcTax () {
-// // // //         console.log("tax rate is 10%");
-// // // //     },
-// // // // };
+// // // // // const karanAujla = { 
+// // // // //     salary : 100000,
+// // // // //     calcTax() {
+// // // // //         console.log("tax rate is 20%");
+// // // // //     }
+// // // // // }
 
-// // // // const karanAujla = { 
-// // // //     salary : 100000,
-// // // //     calcTax() {
-// // // //         console.log("tax rate is 20%");
+// // // // // karanAujla.__proto__ = employee;
+
+
+// // // // class Toyotacar {
+// // // //     constructor(brand, mileage) {
+// // // //         console.log("creating new object");
+// // // //         this.brand = brand;
+// // // //         this.mileage = mileage;
 // // // //     }
-// // // // }
+// // // //     start() {
+// // // //         console.log("start");
+// // // //     } 
 
-// // // // karanAujla.__proto__ = employee;
+// // // //     stop() {
+// // // //         console.log("stop");
+// // // //     }
 
+// // // // }   
 
-// // // class Toyotacar {
-// // //     constructor(brand, mileage) {
-// // //         console.log("creating new object");
-// // //         this.brand = brand;
-// // //         this.mileage = mileage;
+// // // // let fortuner = new Toyotacar("fortuner", 10); //constructor
+// // // // console.log(fortuner);
+// // // // let lexus = new Toyotacar("lexus", 12); //constructor
+// // // // console.log(lexus);
+
+// // // class parent {
+// // //     hello() {
+// // //         console.log("hello");
 // // //     }
-// // //     start() {
-// // //         console.log("start");
-// // //     } 
+// // // }
 
-// // //     stop() {
-// // //         console.log("stop");
-// // //     }
+// // // class child extends parent {}
 
-// // // }   
+// // // let obj = new child();
 
-// // // let fortuner = new Toyotacar("fortuner", 10); //constructor
-// // // console.log(fortuner);
-// // // let lexus = new Toyotacar("lexus", 12); //constructor
-// // // console.log(lexus);
+// // class person{
+// //     constructor(name) {
+// //         console.log("enter parent constructor");
+// //         this.species = "homo sapiens";  
+// //         this.name = name;
+// //     }
+// //     eat() {
+// //         console.log("eat");
+// //     }
 
-// // class parent {
-// //     hello() {
-// //         console.log("hello");
+// //     sleep() {
+// //         console.log("sleep");
+// //     }
+
+// //     work() {
+// //         console.log("do nothing");
+// //     }
+// // } 
+    
+// // class Engineer extends person{
+// //     constructor(branch) {
+          
+// //         super(name);// to invoke parent class constructor    
+// //     }
+// //     work() {
+// //          super.eat();   
+// //         console.log("solve problems, build something");
 // //     }
 // // }
 
-// // class child extends parent {}
+// // class doctor extends person {
+// //     work() {
+// //         console.log("solve problems, build something");
+// //     }
+// // }
 
-// // let obj = new child();
+// // let engobj = new Engineer();
 
-// class person{
-//     constructor(name) {
-//         console.log("enter parent constructor");
-//         this.species = "homo sapiens";  
+
+// let DATA = "secret information";
+
+// class user{
+//     constructor(name, email) {
 //         this.name = name;
+//         this.email = email;
 //     }
-//     eat() {
-//         console.log("eat");
+   
+//     viewdata() {
+//         console.log("data = ", DATA);
 //     }
+// }
 
-//     sleep() {
-//         console.log("sleep");
-//     }
-
-//     work() {
-//         console.log("do nothing");
+// class Admin extends user {
+//     editdata() {
+//         DATA = "some new value"; 
 //     }
 // } 
-    
-// class Engineer extends person{
-//     constructor(branch) {
-          
-//         super(name);// to invoke parent class constructor    
-//     }
-//     work() {
-//          super.eat();   
-//         console.log("solve problems, build something");
-//     }
-// }
 
-// class doctor extends person {
-//     work() {
-//         console.log("solve problems, build something");
-//     }
-// }
+// let student1 = new user("mammu", "mmu@gmail.com");
+// let student2 = new user("abbas", "abu12@gmail.com");
 
-// let engobj = new Engineer();
+// let teacher1 = new user("dean", "dean@clg.com");
 
-
-let DATA = "secret information";
-
-class user{
-    constructor(name, email) {
-        this.name = name;
-        this.email = email;
-    }
-   
-    viewdata() {
-        console.log("data = ", DATA);
-    }
-}
-
-class Admin extends user {
-    editdata() {
-        DATA = "some new value"; 
-    }
-} 
-
-let student1 = new user("mammu", "mmu@gmail.com");
-let student2 = new user("abbas", "abu12@gmail.com");
-
-let teacher1 = new user("dean", "dean@clg.com");
+let a = 5;
+let b = 10;
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a+b = ", a+b);
