d3ac762283aab4a44c3718582fa10c96b02abd88
mohammadhusainmasi <mohammadhusain0538@gmail.com>
2024-12-19 16:14:46 +0530

    api calls 19/12

--- api call/app.js
+++ api call/app.js
@@ -1,4 +1,4 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
+const URL = "https://cat-fact.herokuapp.com/facts/Mammu";
 const factpara = document.querySelector("fact");
 const btn = document.querySelector("btn");
 
@@ -9,7 +9,17 @@ const getFacts = async () => {
     console.log(response); //JSON format 
     let data = await response.json();
 factpara.innerText = data[2],Text;
-}; 
-
+};
+ 
+function getFacts() {
+     fetch(URL)
+     .then((response) => {
+        return response.json();
+     })
+     .then((Data) => {
+        console.log(Data);
+        factpara.innerText = data[2],Text;
+     })
+}
 
 btn.addEventListener("click", getFacts); 
