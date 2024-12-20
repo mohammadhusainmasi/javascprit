1ca7068386d8a3e37a49b9a05b9451c3b1ea116a
mohammadhusainmasi <mohammadhusain0538@gmail.com>
2024-12-20 18:02:49 +0530

    api calls 20/12

--- api call/app.js
+++ api call/app.js
@@ -1,25 +1,52 @@
-const URL = "https://cat-fact.herokuapp.com/facts/Mammu";
-const factpara = document.querySelector("fact");
-const btn = document.querySelector("btn");
+const BASE_URL = 
+"https://2024-03-06.currency-api.pages.dev/v1/currencies/eur.json";
 
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button"); 
+const fromcurr = document.querySelector(".from select");
+const tocurr = document.querySelector(".to select");
 
-const getFacts = async () => {
-    console.log("getting data....");
-    let response = await fetch(URL);
-    console.log(response); //JSON format 
-    let data = await response.json();
-factpara.innerText = data[2],Text;
-};
  
-function getFacts() {
-     fetch(URL)
-     .then((response) => {
-        return response.json();
-     })
-     .then((Data) => {
-        console.log(Data);
-        factpara.innerText = data[2],Text;
-     })
+
+for (let select of dropdowns) {
+    for (currcode in countryList) {
+        let newoption = document.createElement("option");
+        newoption.innerText = currcode;
+        newoption.value = currcode;
+        if(select.name === "from" && currcode === "USD") {
+            newoption.selected = "selected";
+        }else  if(select.name === "to" && currcode === "INR") {
+            newoption.selected = "selected";
+        }
+        select.append(newoption);    
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateflag(evt.target);
+    })
 }
 
-btn.addEventListener("click", getFacts); 
+const updateflag = (element) => {
+    let currcode = element.value;
+    let countrycode = countryList(currcode); 
+    let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+    let img =  element.parentElement.querySelector("img");
+    img.src = newsrc;
+};
+
+btn.addEventListener("click",async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtval = amount.value;
+    if(amtval === "" || amtval < 1) {
+        amtval = 1;
+        amount.value = "1";
+    }
+
+    //console.log(fromcurr.value, tocurr.value);
+    const URL = `${BASE_URL}/${fromcurr.value.tolowercase()}/${tocurr.value.tolowercase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[tocurr.value.tolowercase()]
+    console.log(rate);
+});
